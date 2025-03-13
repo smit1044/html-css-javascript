128b7318775d7e3079a620153e4b5cd396b1eead
smit1044 <smit1044555@gmail.com>
2025-03-13 12:06:56 +0530

    Add files via upload

---
+++ javascript/functions123.js
@@ -0,0 +1,36 @@
+console.log("this is function tutorial");
+function greet(name ,greettext){
+    console.log(name + "learning js");
+    console.log("goodmorning" + "  "+name);
+}
+let name = "smit";
+let name1 = "edfv";
+let name2 = "zvsdb";
+let name3 = "bbsfbsf";
+let greettext = "good morning"
+
+greet(name,greettext);
+greet(name1,greettext);
+greet(name2,greettext);
+greet(name3,greettext);
+greet(name,greettext);
+greet(name1,greettext);
+greet(name2,greettext);
+greet(name3,greettext);
+
+
+
+function sum(a,b,c){
+    let d = a + b + c;
+    return d;
+}
+
+
+
+let returnVal = sum(1,2,3);
+console.log(returnVal)
+
+// console.log(name + "is learning js")
+// console.log(name1 + "is learning js")
+// console.log(name2 + "is learning js")
+// console.log(name3 + "is learning js")
